6364e27c4af40d66080e80ea7c7dc24ed4328f65
JanithaB <jbrathnayake98@gmail.com>
2026-01-20 11:05:52 +0530

    Revert "Merge branch 'dev'"
    This reverts commit c91dabc257e146c614b17ae6df1bbe32499c71dd, reversing
    changes made to 41d65ce645b735766ec1b0737576a7fa436dd08f.

--- scripts/switch-to-client.sh
+++ scripts/switch-to-client.sh
@@ -5,8 +5,6 @@ echo "Switching to Client Mode..."
 # Stop AP services
 sudo systemctl stop hostapd
 sudo systemctl stop dnsmasq
-sudo systemctl disable hostapd
-sudo systemctl disable dnsmasq
 sudo pkill -f portal_server.py
 
 # Clear iptables rules
@@ -19,22 +17,14 @@ sudo netfilter-persistent save
 sudo ip addr flush dev wlan0
 sudo ip link set wlan0 down
 sleep 1
+sudo ip link set wlan0 up
 
 # Re-enable NetworkManager management of wlan0
 sudo rm -f /etc/NetworkManager/conf.d/unmanaged.conf
 sudo systemctl reload NetworkManager
 
-# Wait for NetworkManager to reload
+# Wait for NetworkManager to take over
 sleep 2
-
-# Bring up wlan0 for NetworkManager
-sudo ip link set wlan0 up
-
-# Explicitly tell NetworkManager to manage wlan0
-sudo nmcli device set wlan0 managed yes
-
-# Wait for NetworkManager to fully initialize the interface
-sleep 3
 
 echo "Client Mode activated!"
 echo "NetworkManager will now manage wlan0"
